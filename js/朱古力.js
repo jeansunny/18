ef310c4b6073a56a1a7371ec6b9011e7c80e1d34
jeansunny <jeansunny@126.com>
2024-02-08 03:11:32 +0800

    Update 朱古力.js

--- js/朱古力.js
+++ js/朱古力.js
@@ -6,8 +6,8 @@ var rule = {
         'User-Agent':'MOBILE_UA'
     },
     timeout:5000,
-    class_name:'最新&熱門&每日&精選',//静态分类名称拼接
-    class_url:'最新av線上看&熱門av線上看&每日av線上看線上看&精選av線上看',//静态分类标识拼接
+    class_name:'熱門&無碼',//静态分类名称拼接
+    class_url:'精選av線上看&無碼av線上看',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
